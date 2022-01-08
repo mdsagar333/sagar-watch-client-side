bebb76fba6ddc51c0277d86ed78382f0ef5c7080
mdsagar333 <md_sagar@yahoo.com>
2022-01-08 17:39:58 +0600

    payment compoennt added

---
+++ src/Pages/Dashboard/UserComponent/Payment/CheckoutPayment.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckoutPayment = () => {
+  return <div></div>;
+};
+
+export default CheckoutPayment;
--- src/Pages/Shared/Product/Product.js
+++ src/Pages/Shared/Product/Product.js
@@ -8,7 +8,7 @@ const Product = ({ image, name, price, description, _id, brand, index }) => {
     <Flip left duration={1200} delay={index * 200}>
       <div className="col-12 col-md-3 d-flex align-items-stretch">
         <div className="custom_watch_card">
-          <div className="card ">
+          <div className="card">
             <img src={image} className="card-img-top" alt={name} />
             <div className="card-body text-center">
               <span className="small text-muted">{brand}</span>
@@ -18,13 +18,15 @@ const Product = ({ image, name, price, description, _id, brand, index }) => {
               </p>
               <h6 className="text-muted mb-0">${price}</h6>
             </div>
-            <div className="card-footer border-0 bg-transparent p-0 custom_letter_spacing fw-bold">
-              <Link
-                to={`/product/${_id}`}
-                className="btn text-uppercase btn_custom w-100"
-              >
-                Buy This Product
-              </Link>
+            <div>
+              <div className="card-footer border-0 bg-transparent p-0 custom_letter_spacing fw-bold">
+                <Link
+                  to={`/product/${_id}`}
+                  className="btn text-uppercase btn_custom w-100"
+                >
+                  Buy This Product
+                </Link>
+              </div>
             </div>
           </div>
         </div>
